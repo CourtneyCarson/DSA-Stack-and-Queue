a720e9d56a73bc523e26457ad8daa748e43a3449
Courtney Carson <cmacarson@gmail.com>
2020-12-04 08:23:58 -0800

    1st question

--- stacks.js
+++ stacks.js
@@ -1,27 +1,47 @@
 class _Node {
   constructor(data, next) {
-    this.data = data; 
+    this.data = data;
     this.next = next;
   }
 }
 
 class Stack {
   constructor() {
-    this.top = null; 
+    this.top = null;
+  }
+
+
+  push(data) {
+    if (this.top === null) {
+      this.top = new _Node(data, null);
+      return this.top;
+    }
+    const node = new _Node(data, this.top);
+    this.top = node;
+  }
+
+  Pop() {
+    const node = this.top;
+    this.top = node.next;
+    return node.data;
   }
 }
 
-push(data) {
-  if (this.top === null) {
-    this.top = new _Node(data, null);
-    return this.top;
-  }
+////////////////////////// DRILLS /////////////////////////////
+
+
+// 1. Create a stack called starTrek and add;
+// Kirk, Spock, McCoy, and Scotty to the stack.
+function newFunction() {
+  const starTrek = new Stack();
+  starTrek.push("kirk");
+  starTrek.push("Spock");
+  starTrek.push("McCoy");
+  starTrek.push("Scotty");
+  
+
+  console.log(starTrek);
+  console.log(starTrek.top.next);
+
 }
-
-Pop() {
-  const node = this.top;
-  this.top = node.next; 
-  return node.data; 
-}
-
-
+ newFunction(); 
