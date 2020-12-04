1e9c4445d530ea7816bc79cde39e81c46c009812
Courtney Carson <cmacarson@gmail.com>
2020-12-04 13:08:00 -0800

    question 4 completed

--- stacks.js
+++ stacks.js
@@ -46,7 +46,7 @@ class Stack {
       currentNode = currentNode.next;
     }
   }
-  
+
 }
 
 
@@ -67,11 +67,11 @@ function newFunction() {
   starTrek.peek();
   starTrek.display();
   //2.2: Remove McCoy from your stack and display the stack
-  starTrek.Pop()
-  starTrek.Pop()
+  starTrek.Pop();
+  starTrek.Pop();
 
-  console.log(starTrek.Pop())
-  console.log(starTrek.Pop())
+  console.log(starTrek.Pop());
+  console.log(starTrek.Pop());
   // console.log(starTrek.peek())
   // console.log(starTrek.isEmpty());
   // console.log(starTrek);
@@ -85,6 +85,9 @@ newFunction();
 // peek(): allows you to look at the top of the stack without removing it
 // isEmpty(): allows you to check if the stack is empty or not
 // display(): to display the stack - what is the 1st item in your stack?
+
+
+
 
 
 // 3. Check for palindromes using a stack
@@ -101,7 +104,7 @@ function is_palindrome(string) {
   // Your code goes here
   let reverseString = string.split('').reverse().join('');
 
-  return reverseString === string; 
+  return reverseString === string;
 }
 
 // True, true, true, false
@@ -109,3 +112,66 @@ console.log(is_palindrome("dad"));
 console.log(is_palindrome("A man, a plan, a canal: Panama"));
 console.log(is_palindrome("1001"));
 console.log(is_palindrome("Tauhida"));
+
+
+// 4. Matching parentheses in an expression
+// A stack can be used to ensure that an arithmetic expression has balanced parentheses.;
+// Write a function that takes an arithmetic expression as an argument and returns true or;
+// false based on matching parenthesis.As a bonus provide a meaningful error message to the user;
+// as to what's missing. For example, you are missing a ( or missing a ")".;;;
+
+function matchingParenthesis(string) {
+  const stack = new Stack;
+  //set an open array & starting index
+  let openingIndex = [];
+  let index = 0;
+  // loop through string 
+  for (let char of string) {
+    // if characters of [ ( ] are a match push char into stack, and push index into opening index
+    if (char.match(/[(]/g)) {
+      stack.push(char);
+      openingIndex.push(index);
+    }
+
+    // if characters of [ ) ] are a match g (global) returns all matches, instead of stopping after the first match
+    if (char.match(/[)]/g)) {
+      // if there is nothing or the value is not '(' return missing statement 
+      if (!stack.peek() || !stack.peek().value === '(') {
+        return 'Missing ( at index ' + index;
+      }
+      // else pop - remove last item 
+      else {
+        stack.Pop();
+        // .pop is its own method not the one I made above 
+        openingIndex.pop();
+      }
+    }
+
+
+    // increment index 
+    index++;
+  }
+
+  // if stack is empty return error + index.length -1 
+  if (!stack.isEmpty()) {
+
+    console.log('Missing ) at index ' + openingIndex[openingIndex.length - 1]);
+    return 'Missing ) at index ' + openingIndex[openingIndex.length - 1];
+  }
+
+  console.log('All parenthesis are closed');
+
+  return 'All parenthesis are closed';
+
+
+}
+matchingParenthesis('(1)+ 1');
+matchingParenthesis('(1 + 1');
+
+//5. Sort stack
+// Write a program to sort a stack such that the smallest items are on the top(in ascending order).;
+// You can use an additional stack, but you may not use any other data structure(such as an array, 
+// or linked list).
+
+
+
