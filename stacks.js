edc5a933147f5612fd64591475fc86c805076884
Courtney Carson <cmacarson@gmail.com>
2020-12-04 09:08:48 -0800

    question 2

--- stacks.js
+++ stacks.js
@@ -1,3 +1,4 @@
+//1. write a Stack class with its core functions (push, pop) from scratch.
 class _Node {
   constructor(data, next) {
     this.data = data;
@@ -25,7 +26,23 @@ class Stack {
     this.top = node.next;
     return node.data;
   }
+  //allows you to look at the top of the stack without removing it
+  peek() {
+    return this.top.data;
+  }
+  //: allows you to check if the stack is empty or not
+  isEmpty() {
+    if (!this.top) {
+      return true
+    } else {
+      return false
+    }
+  }
 }
+
+
+
+
 
 ////////////////////////// DRILLS /////////////////////////////
 
@@ -38,10 +55,24 @@ function newFunction() {
   starTrek.push("Spock");
   starTrek.push("McCoy");
   starTrek.push("Scotty");
+  starTrek.peek()
   
-
-  console.log(starTrek);
-  console.log(starTrek.top.next);
+  // console.log(starTrek.peek())
+  console.log(starTrek.isEmpty())
+  // console.log(starTrek);
+  // console.log(starTrek.top.next);
 
 }
- newFunction(); 
+newFunction();
+
+//2. Using the Stack class above, implement 
+// the following helper functions outside of the class:
+// peek(): allows you to look at the top of the stack without removing it
+// isEmpty(): allows you to check if the stack is empty or not
+// display(): to display the stack - what is the 1st item in your stack?
+
+
+
+
+
+// };
